7eef93cbdeef4029eec102a13f46d1659d40b3a4
Joao Victor S. Cavalcante <joaovictors.cavalcante1@gmail.com>
2022-08-10 12:39:31 -0300

    modificaçoes:(arquivo animal), (classe Animal), (linha 11 a 36)

--- animal.py
+++ animal.py
@@ -1,12 +1,49 @@
 class Animal():
-    def __init__(self, idade, raca, sexo, altura, deficiencia):
+    def __init__(self, nome,idade, raca, sexo, porte_animal, deficiencia,vacina):
+        self.nome=nome
         self.idade=idade
         self.raca=raca
         self.sexo=sexo
-        self.altura=altura
+        self.porte_animal=porte_animal
         self.deficiencia=deficiencia
+        self.vacinacao=vacina
 
-#         animalOb = animal.Animal(idade, raca, sexo, altura, deficiencia
+        def __init__(self,nome):
+            self.nome=nome
+
+            veri_coleira=input('se o animal possui coleira digite (S), senao (N)')
+
+            if(veri_coleira =='s'or veri_coleira=='S'):
+                print('digite o nome do animal')
+                nome=input('')
+
+
+            else:
+                print('continue o cadastro')
+
+    def vacin_animais (self):
+
+        print('digite (S) se  animal é vacinado\n digite(N) se o animal não for vacinado\n ')
+        print('digite(NS) se não ha informações se o animal é vacinado ')
+        veri_vacina=input('')
+
+        if (veri_vacina=='s'or veri_vacina=='S'):
+            print('quantas vacinas oanimal tomo')
+            tipos_vacina=[]
+            quantidade_vacina=int(input(''))
+
+            print('quais foram as vacinas que o animal tomou')
+            tipos_vacina.append(input(''))
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+
+
+import animal
+
+objeto_animal=animal.Animal(self,idade,raca,sexo, porte_animal, deficiencia)
+
 print("ola mundo")
 print('joao push')
