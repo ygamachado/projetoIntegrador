a681f475968ffc26da7a957d93c3afc96950feac
Joao Victor S. Cavalcante <joaovictors.cavalcante1@gmail.com>
2022-08-23 11:46:20 -0300

    parei na disponibilidade do animal para a adoçao

--- animal.py
+++ animal.py
@@ -17,7 +17,7 @@ class Animal ():
         self.porte_animal = porte_animal
         self.deficiencia = deficiencia
         self.vacinacao = vacina
-
+# INDICE DA FUNLAO DE CADASTRO DE ANIMAIS
 
     def cadastroanimal(self):
         # nome do animal se tiver uma colera e tiver o nome na coleira tbm
@@ -49,19 +49,54 @@ class Animal ():
     print(' porte do animal\n piqueno porte digite: (P)\n medio porte digite: (M)\n auto porte digite (A)')
     veriporte=input('digite:')
 
-    if (veriporte=='p') or (veriporte=='P'):
-        porte='P'
+        if (veriporte=='p') or (veriporte=='P'):
 
-    elif (veriporte=='m') or (veriporte=='M'):
-        porte='M'
+            porte='P'
 
-    elif (veriporte=='a') or (veriporte=='A'):
-        porte='a'
+        elif (veriporte=='m') or (veriporte=='M'):
 
-    else:
-        print('ERRO')
-        print('NENHUMA DAS OPÇÕ~ES FIU SELECINADAS')
-        print('volte ao menu com a tecla ()')
+            porte='M'
+
+        elif (veriporte=='a') or (veriporte=='A'):
+
+            porte='a'
+
+
+        else:
+            print('ERRO')
+            print('NENHUMA DAS OPÇÕES FIU SELECINADAS')
+            print('volte ao menu com a tecla ()')
+
+    # RAÇA DO ANIMAL
+    print('digite a raça/especie do animal')
+    raca=input('digite:')
+
+    # idade estimada do animal
+    print('idade estimada do animal')
+    idade=('digite:')
+
+    # DISPONIVEL PARA DOAÇAO
+
+    print('disponivel para a doeção S/N')
+    veridis=('digite:')
+
+
+        if (veridis == 's') or (veridis == 'S'):
+
+            disponivel = 'S'
+
+
+        elif(veridis=='n') or (veridis=='N'):
+
+            disponivel='N'
+
+
+        else:
+            print('ERRO')
+            print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+
+
+
 
 
 
