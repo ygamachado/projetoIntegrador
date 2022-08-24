e66d18bfe3ff27a6d6d686edecb9b3a292f05c86
Joao Victor S. Cavalcante <joaovictors.cavalcante1@gmail.com>
2022-08-24 15:20:56 -0300

    terminar de enserir os comandos do mysql e colocar os objetos nos locais certos no final do cadastro

--- animal.py
+++ animal.py
@@ -17,51 +17,56 @@ class Animal ():
         self.porte_animal = porte_animal
         self.deficiencia = deficiencia
         self.vacinacao = vacina
+
 # INDICE DA FUNLAO DE CADASTRO DE ANIMAIS
 
     def cadastroanimal(self):
 
-        # nome do animal se tiver uma colera e tiver o nome na coleira tbm
+
+
+    # nome do animal se tiver uma colera e tiver o nome na coleira tbm
         conexao = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='ong'
+        host='localhost',
+        user='root',
+        password='',
+        database='ong'
 
         )
-        cursor=conexao.cursor()
+        cursor = conexao.cursor()
 
         print(' CADASTRO DE ANIMAIS')
         print('')
 
         print('o animal pssui nome? s/n')
-        vericada=input('digite:')
+        vericada = input('digite:')
 
-        if (vericada=='s') or (vericada=='S'):
+        if (vericada == 's') or (vericada == 'S'):
             print('digte o nome do animal')
-            nomeanimal=input('digite:')
+            nomeanimal = input('digite:')
 
         else:
             print('')
-    # cor do animal
-    print('digite a cor do animal')
-    coranimal=("digte:")
-    print('')
-        # porte do animal
-    print(' porte do animal\n piqueno porte digite: (P)\n medio porte digite: (M)\n auto porte digite (A)')
-    veriporte=input('digite:')
 
-        if (veriporte=='p') or (veriporte=='P'):
 
-            porte='P'
+# cor do animal
+        print('digite a cor do animal')
+        coranimal = ("digte:")
+        print('')
+# porte do animal
+        print(' porte do animal\n piqueno porte digite: (P)\n medio porte digite: (M)\n auto porte digite (A)')
+        veriporte = input('digite:')
 
-        elif (veriporte=='m') or (veriporte=='M'):
+        if (veriporte == 'p') or (veriporte == 'P'):
 
-            porte='M'
+            porte = 'P'
 
-        elif (veriporte=='a') or (veriporte=='A'):
+        elif (veriporte == 'm') or (veriporte == 'M'):
 
-            porte='a'
+            porte = 'M'
+
+        elif (veriporte == 'a') or (veriporte == 'A'):
+
+            porte = 'a'
 
 
         else:
@@ -69,50 +74,94 @@ class Animal ():
             print('NENHUMA DAS OPÇÕES FIU SELECINADAS')
             print('volte ao menu com a tecla ()')
 
-    # RAÇA DO ANIMAL
-    print('digite a raça/especie do animal')
-    raca=input('digite:')
+# RAÇA DO ANIMAL
+        print('digite a raça/especie do animal')
+        raca = input('digite:')
 
-    # idade estimada do animal
-    print('idade estimada do animal')
-    idade=('digite:')
+# idade estimada do animal
+        print('idade estimada do animal')
+        idade = ('digite:')
 
-    # DISPONIVEL PARA DOAÇAO
+# DISPONIVEL PARA DOAÇAO
 
-    print('disponivel para a doeção S/N')
-    veridis=('digite:')
-
+        print('disponivel para a doeção? S/N')
+        veridis = ('digite:')
 
         if (veridis == 's') or (veridis == 'S'):
+            disponivel = 'DISPONIVEL'
 
-            disponivel = 'S'
 
+        elif (veridis == 'n') or (veridis == 'N'):
 
-        elif(veridis=='n') or (veridis=='N'):
-
-            disponivel='N'
+            disponivel = 'INDISPOIVEL'
 
 
         else:
             print('ERRO')
             print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+# deficincia
 
-# teno que dar 9 espaço para dicar no espaçamento serto
+        print('o animal pessui possui alguma deficciencia fisica? s/n')
+        veridef=input('digite:')
+
+        if (veridef=='s') or (veridis=='S'):
+                print('digtie a deficencia fisica do animal')
+                deficiencia=input(('digite:'))
+
+        elif (veridef=='n') or (veridis=='N'):
+            deficiencia='sem dificiencia'
+
+        else:
+            print('ERRO')
+
+            print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+
+# animal castardo
+        print('animal é castrado? S/N')
+        castrado=('digite:')
+
+#  exames animal
+        print('o animal a fez algum exame? S/N')
+        veriex=input('digite')
+
+        if (veriex=='s') or (veriex=='S'):
+            print('quantos?')
+            enxames=input(('digite:'))
+
+        elif (veriex=='n') or (veriex=='N'):
+            enxames= 'sem exames'
+
+        else:
+            print('ERRO')
+            print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+
+# local que o animal foi encotrado
+        print('qual foi o local que o animal foi encontrado/resgatado')
+        localecontrado=input('digite:')
+
+# data que o animal foi encontrado
+
+        print('data que o animal foi encotrado')
+        dataresgate=input('digite:')
+
+        comando=f'insert into cad_animal(id_exame, nome, cor, porte, raça, idade, disponivel, adotado, deficiente, castrado, quant_exames,local_encontro, data_encontro) values('1 ', 'chulapa', 'preto', 'p', 'pinsher', '2', 'n', 'n', 'n', 's', '0', 'Rua Castroalves', 20220218);'
+
+
+
+
+
+
+
+    # teno que dar 9 espaço para dicar no espaçamento serto
 # espaçamento esta errado
-
-
-
-
-
-
-
-
 
 
 
 
         cursor.close()
         conexao.close()
+
+    cadastroanimal()
 
 
 
