33a9706bc1cf82156fcc27c47d59ae7a1b0c1bc9
Joao Victor S. Cavalcante <joaovictors.cavalcante1@gmail.com>
2022-08-27 12:26:43 -0300

    ,

--- main.py
+++ main.py
@@ -33,3 +33,162 @@ def cadastrando_adotante():
         dados_ad.cadastro_adotante(id_adotante, nome, nascimento, telefone1, telefone2, email, usuario, senha)
     else:
         print("\n===================== Os dados não foram salvos =====================")
+
+
+
+def cadastroanimal():
+
+    print(' CADASTRO DE ANIMAIS')
+    print('')
+
+
+    # n sei ara q serve esse id exame
+    print('digite o exame valor a enserir terar de ser inteiro')
+    idexame = int(input('digite:'))
+
+    print('')
+
+
+    # nome do animal se tiver
+    print('o animal possui nome? s/n')
+    verino = input('digite:')
+
+    if (verino == 's') or (verino == 'S'):
+        print('digite o nome que esta na coleira:')
+        nomeanimal = input('digite:')
+
+    elif (verino == 'n') or (verino == 'N'):
+        nomeanimal='N'
+
+
+    else:
+        print('ERRO')
+        print('NENHUMA DAS OPÇÕES FOI SELECINADAS')
+        print('volte ao menu com a tecla ()')
+    print('')
+
+
+    # cor do animal
+    print('digite a cor do animal')
+    coranimal =input("digite:")
+    print('')
+
+
+    # porte do animal
+    print(' porte do animal:\n piqueno porte digite: (P)\n medio porte digite: (M)\n auto porte digite (A)')
+    veriporte = input('digite:')
+
+    if (veriporte == 'p') or (veriporte == 'P'):
+
+        porte = 'P'
+
+    elif (veriporte == 'm') or (veriporte == 'M'):
+
+        porte = 'M'
+
+    elif (veriporte == 'a') or (veriporte == 'A'):
+
+        porte = 'A'
+
+
+    else:
+        print('ERRO')
+        print('NENHUMA DAS OPÇÕES FOI SELECINADAS')
+        print('volte ao menu com a tecla ()')
+
+    print('')
+
+    # RAÇA DO ANIMAL
+    print('digite a raça/especie do animal')
+    raca = input('digite:')
+    print('')
+
+
+    # idade estimada do animal
+    print('idade estimada do animal')
+    idade =input('digite:')
+    print('')
+
+
+    # DISPONIVEL PARA DOAÇAO
+
+    print('disponivel para a doeção? S/N')
+    veridis=input('digite:')
+
+    if (veridis == 's') or (veridis == 'S'):
+        disponivel = 'S'
+
+
+    elif (veridis == 'n') or (veridis == 'N'):
+
+        disponivel = 'N'
+
+
+    else:
+        print('ERRO')
+        print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+    print('')
+
+
+
+# adotado?
+    print('o animal ja foi adotado? S/N')
+    adotado = input('digite:')
+    print('')
+
+
+    # deficincia
+
+    print('o animal possui alguma deficiencia fisica? s/n')
+    veridef = input('digite:')
+
+    if (veridef == 's') or (veridef == 'S'):
+
+        deficiencia ='S'
+
+    elif (veridef == 'n') or (veridef == 'N'):
+        deficiencia = 'N'
+
+    else:
+        print('ERRO')
+
+        print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+    print('')
+
+
+    # animal castardo
+    print('animal é castrado? S/N')
+    castrado =input ('digite:')
+    print('')
+
+
+    #  exameas animal
+    print('o animal a fez algum exame? S/N')
+    veriex = input('digite:')
+
+    if (veriex == 's') or (veriex == 'S'):
+        print('quantos?')
+        enxames = int(input(('digite:')))
+
+    elif (veriex == 'n') or (veriex == 'N'):
+        enxames = 0
+
+    else:
+        print('ERRO')
+        print('COMANDO NÃO FOI SEGUIDO CORRETAMENTE')
+    print('')
+
+
+    # local que o animal foi encotrado
+    print('qual foi o local que o animal foi encontrado/resgatado')
+    localecontrado = input('digite:')
+    print('')
+
+
+    # data que o animal foi encontrado
+
+    print('data que o animal foi encotrado')
+    dataresgate = input('digite:')
+    print('')
+
+    print('CADASTRO C0NCLUIDO COM SUCESSO !!')
